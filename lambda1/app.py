4ca5c29799a2159c3eaeb6276d7b9cbbcabd4506
Rohan Khanal <125800654+RohanKhanal14@users.noreply.github.com>
2026-01-26 16:23:15 +0545

    gvgh

--- lambda1/app.py
+++ lambda1/app.py
@@ -10,7 +10,7 @@ def lambda_handler(event, context):
     return {
         'statusCode': 200,
         'body': json.dumps({
-            'message': 'Hello from test !',
+            'message': 'Hello fromt !',
             'lambda': 'lambda1'
         })
     }
