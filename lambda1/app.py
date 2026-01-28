cef4461b0c075ff02785e054394a194104c7af7c
rohan.khanal <rohan.khanal@genesesolution.com>
2026-01-28 16:22:35 +0545

    test deploy

--- lambda1/app.py
+++ lambda1/app.py
@@ -1,4 +1,5 @@
 import json
+import requests
 from logger import get_logger
 
 logger = get_logger(__name__)
@@ -6,12 +7,14 @@ logger = get_logger(__name__)
 def lambda_handler(event, context):
     logger.info("Lambda 1 invoked")
     logger.info(f"Event: {json.dumps(event)}")
+    response = requests.get("https://httpbin.org/get", timeout=3)
+
     
     return {
         'statusCode': 200,
         'body': json.dumps({
             'message': 'Hello from test for webhook! this si branch tt',
-            
+            'http_status': response.status_code,
             'lambda': 'lambda1'
         })
     }
